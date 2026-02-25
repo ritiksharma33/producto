053285e90aefcdd8c5bfad6ee047fb4b74fd9679
ritiksharma33 <ritikeducationalid@gmail.com>
2026-02-25 12:52:48 +0530

    sercured the routess

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton } from '@clerk/clerk-react'
 import React from 'react'
-import { Route, Routes } from 'react-router'
+// Added 'Navigate' to the import list below
+import { Route, Routes, Navigate } from 'react-router'
 import Navbar from './components/Navbar'
 import ProductPage from './pages/ProductPage'
 import HomePage from './pages/HomePage'
@@ -9,12 +10,16 @@ import CreatePage from './pages/CreatePage'
 import EditProductPage from './pages/EditProductPage'
 import useUserSync from './hooks/useUserSync'
 import useAuthReq from './hooks/useAuthReq'
+
 const app = () => {
   //we are callling the useauth and user sync here and also desrcutuing them 
-  const {isClerkLoaded}=useAuthReq();
+  // Added 'isSignedIn' to the destructuring below so your protected routes work
+  const { isClerkLoaded, isSignedIn } = useAuthReq(); 
   useUserSync();
   //console.log({isSignedIn});
+  
   if(!isClerkLoaded) return null;
+  
   return (
     <div className="min-h-screen bg-base-100">
       <Navbar/>
@@ -23,14 +28,14 @@ const app = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          {/* if user signed in then only open this page else navigate to the signin tab home tab  */}
+          <Route path="/profile" element={isSignedIn?<ProfilePage />:<Navigate to={"/"}/>} />
+          {/* if user signed in then only open this page else navigate to the signin tab home tab  we haev protected all the pages*/}
           <Route path="/create" element={isSignedIn?<CreatePage />:<Navigate to={"/"}/>} />
-          <Route path="/edit/:id" element={<EditProductPage />} />
+          <Route path="/edit/:id" element={isSignedIn?<EditProductPage />:<Navigate to={"/"}/>} />
 
         </Routes>
       </main>
-      x
+      
     </div>
   )
 }
